305e02de5f80dc374fe79cd7d7109605b52e2d81
Ashley <a.clarkem@gmail.com>
2014-06-19 14:28:11 -0600

    print first genres

--- 02_first_genre.rb
+++ 02_first_genre.rb
@@ -1,3 +1,6 @@
 require_relative 'people'
 
 # What is the first genre that each person listed?
+
+puts PEOPLE["joe"][:preferences][:favorite_genres][0]
+puts PEOPLE["sue"][:preferences][:favorite_genres][0]
